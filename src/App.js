dc3b0a87ed17af6e327077e1b28945c1218d83e2
Bruno Valle <brunolv6@gmail.com>
2019-07-30 21:53:38 -0300

    Add React-Router for simple test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+
 //por ser classe Homepage, NÃO deve tem que ter { ... }
 import Homepage from "./pages/homepage/homepage.component.jsx"
 
 import './App.scss';
 
+const Hats = () => {
+  return (
+    <div>
+      <Link to='/'>Voltar</Link>
+      <div>Aqui tem Chapéus!!!</div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/hats' component={Hats}/>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.style.scss'
 
-export const MenuItem = ({title, imageUrl, size}) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
     return(
-        <div className={`${size} menu-item`}>
+        <div 
+            className={`${size} menu-item`}
+            onClick={()=>history.push(`${match.url}${linkUrl}`)}
+        >
             <div
             className='background-image'
             style={{
@@ -12,9 +16,11 @@ export const MenuItem = ({title, imageUrl, size}) => {
             }}
             />
             <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>SHOP NOW</span>
             </div>
         </div>
     );
 }
+
+export default withRouter(MenuItem);
