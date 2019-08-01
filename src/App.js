73fa6da3ca0bd13863982ba7d44eb4209eb39d90
Bruno Valle <brunolv6@gmail.com>
2019-08-01 10:08:57 -0300

    Header component and a little of style

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Route, Switch, Link } from 'react-router-dom';
 //por ser classe Homepage, NÃO deve tem que ter { ... }
 import Homepage from "./pages/homepage/homepage.component.jsx"
 import ShopPage from './pages/shop-page/shop-page.component.jsx'
+import Header from './components/header/header.component'
 
 import './App.scss';
 
 function App() {
   return (
     <div>
-      <Link to='/'>Voltar</Link>
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/shop' component={ShopPage}/>
