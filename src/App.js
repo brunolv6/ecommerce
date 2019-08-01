3a36aae11e9547e59232caeba5a79bf0dddc3ece
Bruno Valle <brunolv6@gmail.com>
2019-08-01 11:24:56 -0300

    Style rest of Header, padding for Header and between Collection-Preview

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 //por ser classe Homepage, NÃO deve tem que ter { ... }
 import Homepage from "./pages/homepage/homepage.component.jsx"
@@ -10,7 +10,7 @@ import './App.scss';
 
 function App() {
   return (
-    <div>
+    <div className='app'>
       <Header/>
       <Switch>
         <Route exact path='/' component={Homepage}/>
